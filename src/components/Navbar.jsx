4bd581b09627724eb2a30cf8e06f49acc05ae7c1
Abdelrahman Ashraf <abdelrahmanashraf6000@gmail.com>
2025-09-10 21:02:52 +0300

    bug: fix url issue

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import "../styles/navbar.css"
+import { Link } from "react-router-dom"
 
 function Navbar() {
-    return <nav className="flex flex-row justify-between text-center z-10 bg-white items-center sticky top-0 shadow-sm px-5 sm:px-10 py-3">
-        <div className="nav-brand text-xl font-semibold cursor-pointer text-red-600 transition duration-500"><a href="/">Movie Go</a> </div>
-        <div className="nav-links flex gap-10">
-            <a href="/" className="nav-item hover:text-red-600 transition duration-150">Home</a>
-            <a href="/movie-go/favorite" className="nav-item hover:text-red-600 transition duration-150">Favorites</a>
-        </div>
-    </nav>
+    return (
+        <nav className="flex flex-row justify-between text-center z-10 bg-white items-center sticky top-0 shadow-sm px-5 sm:px-10 py-3">
+            <div className="nav-brand text-xl font-semibold cursor-pointer text-red-600 transition duration-500">
+                <Link to="/movie-go/">Movie Go</Link>
+            </div>
+            <div className="nav-links flex gap-10">
+                <Link to="/movie-go" className="nav-item hover:text-red-600 transition duration-150">Home</Link>
+                <Link to="/movie-go/favorite" className="nav-item hover:text-red-600 transition duration-150">Favorites</Link>
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
